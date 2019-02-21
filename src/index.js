fefd188966d9004c27a0744c29f664c6c847d4b8
Pitipong Guntawong <pppstudio.gm@gmail.com>
2019-02-21 14:31:30 +0700

    Update index.js
    width height support

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const url = require('url');
 const {errorResponse} = require("./response");
+const url = require('url');
 const {original, resize} = require("./image");
 
 exports.handler = (event) => new Promise((resolve, reject) => {
@@ -9,20 +9,42 @@ exports.handler = (event) => new Promise((resolve, reject) => {
         return reject(`Error: Set environment variable IMAGE_BUCKET`);
     }
 
-    const path = event.path;
-    const objectKey = url.parse(path).pathname.replace(/^\/+/g, '');
-    console.log('INFO: key: ' + objectKey);
+    // let path = event.path;
+    // path = path.replace("resize/","media/")
+    // const objectKey = url.parse(path).pathname.replace(/^\/+/g, '');
+    // console.log('INFO: key: ' + objectKey);
 
     const queryParameters = event.queryStringParameters || {};
+     
+    let fullpath = event.path;
+    fullpath = fullpath.replace("resize/","media/")
+    let path = fullpath
+    let extract = fullpath.match(/^(.+)\.thumbnail(\d+)(x(\d+))?$/)
+    let width = 0
+    let height = 0
+    if(extract){
+      path = extract[1]
+      if(extract[2]*1>0){
+        width = extract[2]*1
+      }
+      if(extract[4]*1>0){
+        height = extract[4]*1
+      }
 
-    if (!queryParameters.width && !queryParameters.height) {
+    }else{
+        width = parseInt(queryParameters.width);
+        height = parseInt(queryParameters.height);
+  
+    }
+    
+    const objectKey = url.parse(path).pathname.replace(/^\/+/g, '');
+
+    if (width==0 && height==0) {
         return original(imageBucket, objectKey)
             .then(resolve)
             .catch(reject);
     }
 
-    const width = parseInt(queryParameters.width);
-    const height = parseInt(queryParameters.height);
 
     if ((queryParameters.width && isNaN(width)) || (queryParameters.height && isNaN(height))) {
         return reject(errorResponse(`width and height parameters must be integer`, 400));
